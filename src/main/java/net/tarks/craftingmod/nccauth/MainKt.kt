85ef0848da1ca113f296f18faedf0794e8755265
Craftingmod <craftingmod@tarks.net>
2018-03-06 18:00:00 +0900

    Dirty

--- src/main/java/net/tarks/craftingmod/nccauth/MainKt.kt
+++ src/main/java/net/tarks/craftingmod/nccauth/MainKt.kt
@@ -2,9 +2,14 @@ package net.tarks.craftingmod.nccauth
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
 import org.fusesource.jansi.AnsiConsole
 import org.fusesource.jansi.Ansi.*
 import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
 
 fun main(args:Array<String>){
     AnsiConsole.systemInstall()
@@ -18,18 +23,18 @@ fun main(args:Array<String>){
             reset()
             System.out.println(this)
         }
-        return
+        return;
     }
     ansi().run {
         fgDefault()
         a("Config path: ${configF.absolutePath}, exist: ${configF.exists()}")
         System.out.println(this)
     }
-
-    val main:MainKt = MainKt(configPath = configF)
+    val cafeLink:String? = args.getOrNull(0)
+    val main:MainKt = MainKt(configFile = configF,cafeLink = cafeLink)
 }
 
-class MainKt(val configPath:File, val cafeLink:String? = null) {
+class MainKt(val configFile:File,var cafeLink:String? = null) {
     companion object {
         fun getDefaultDIR():File? {
             val root:File? = Util.getRootdir()
@@ -41,7 +46,18 @@ class MainKt(val configPath:File, val cafeLink:String? = null) {
         }
     }
     val g:Gson = GsonBuilder().create()
+    var config:Config = Config("token","url")
     init {
-        println("${configPath.absolutePath}")
+        if(configFile.exists() && configFile.canRead()){
+            val reader = JsonReader(InputStreamReader(FileInputStream(configFile), Charset.forName("utf-8")))
+            config.copyFrom(g.fromJson<Config>(reader, object : TypeToken<Config>() {}.type))
+        }else{
+            if(configFile.parentFile.canWrite()){
+            }
+        }
+        println("${configFile.absolutePath}")
+    }
+    fun initFromCafe(cafeURL:String) {
+
     }
 }
